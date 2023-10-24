352a915d772ff57e34b1360cf0786d44b9083259
atnbn <altan.bnm@gmail.com>
2023-10-24 19:05:51 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { UranusComponent } from './features/uranus/uranus.component';
 import { NeptuneComponent } from './features/neptune/neptune.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'mercury', pathMatch: 'full' },
+  { path: '**', redirectTo: 'mercury', pathMatch: 'full' },
   { path: 'mercury', component: MercuryComponent },
   { path: 'venus', component: VenusComponent },
   { path: 'earth', component: EarthComponent },
